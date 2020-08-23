88758ab9670ebb882075a1dc40587fe4f690fcb7
JordiRodriguezC <ycorrales120@gmail.com>
2020-08-22 22:44:45 -0600

    :boom:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,11 +17,11 @@
             @auth
             <a href="{{ url('/home') }}">Home</a>
             <a href="{{ url('/usuarios') }}">Mi usuario</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
             @if (Route::has('register'))
             <a href="{{ route('register') }}">Register</a>
             @endif
+            @else
+            <a href="{{ route('login') }}">Login</a>
             @endauth
         </div>
         @endif
