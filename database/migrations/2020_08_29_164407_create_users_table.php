4e3b5f4dee9a2c4a6f85efd0614ada62635a5f30
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-08-29 11:12:38 -0600

    act #8 😇
    hice 3 migraciones (users, tbl_expediente, tbl_antecedentesginecologicos)

--- database/migrations/2020_08_29_164407_create_users_table.php
+++ database/migrations/2020_08_29_164407_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('tipoUsuario');
+            $table->string('cedula');
             $table->rememberToken();
             $table->timestamps();
         });
