bc5c56818e61cee7f843406d07576217184c8a9c
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-09-01 14:50:30 -0600

    act #17💪🏻
    Arreglo de css - error que había con librerías

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,11 @@
     <!-- Styles -->
     <link href='fullcalendar/main.css' rel='stylesheet' />
     <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 </head>
 
