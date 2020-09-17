963e431fb93252d327d30780da5c75c732f89801
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-09-17 17:47:26 -0600

    act #32 :100:
    modelo de antecedentes heredo Familiares y cambio de nombre de tabla relacional entre expediente y antecedentes

---
+++ app/tbl_heredofamiliares.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_pacientes extends Model
+{
+  protected $primaryKey = 'hf_id';
+  protected $fillable = ['hf_nombre'];
+  protected $dates = ['created_at', 'updated_at'];
+}
