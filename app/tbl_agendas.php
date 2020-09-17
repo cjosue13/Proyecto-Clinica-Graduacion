88ee0209d29c705088d91bedf789b1f7c773f73c
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-09-17 17:22:09 -0600

    act #27
    modelo antecedentes

--- app/tbl_agendas.php
+++ app/tbl_agendas.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_agendas extends Model
 {
   protected $primaryKey = 'agn_id';
-    protected $fillable = ['agn_NombreCompleto','agn_telefono','agn_fecha','agn_HoraInicio', 'agn_HoraFinal', 'agn_estado','agn_Tiempo','agn_descripcion'];
-    protected $dates = ['created_at', 'updated_at'];
-
+  protected $fillable = ['agn_NombreCompleto', 'agn_telefono', 'agn_fecha', 'agn_HoraInicio', 'agn_HoraFinal', 'agn_estado', 'agn_Tiempo', 'agn_descripcion'];
+  protected $dates = ['created_at', 'updated_at'];
+  
 }
-
--- app/tbl_pacientes.php
+++ app/tbl_pacientes.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
@@ -6,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_pacientes extends Model
 {
   protected $primaryKey = 'pac_id';
-    protected $fillable = ['pac_pNombre','pac_sNombre', 'pac_pApellido', 'pac_sApellido', 'pac_Cedula', 'pac_Genero', 'pac_FechaNacimiento', 'pac_Residencia', 'pac_Correo', 'pac_Profesion_Oficio', 'pac_EstadoCivil', 'pac_Religion'];
-    protected $dates = ['created_at', 'updated_at'];
-
+  protected $fillable = ['pac_pNombre', 'pac_sNombre', 'pac_pApellido', 'pac_sApellido', 'pac_Cedula', 'pac_Genero', 'pac_FechaNacimiento', 'pac_Residencia', 'pac_Correo', 'pac_Profesion_Oficio', 'pac_EstadoCivil', 'pac_Religion'];
+  protected $dates = ['created_at', 'updated_at'];
 }
-
