66dc715701446f922f0576b6d05986f94aea97f1
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-09-17 17:04:49 -0600

    act #25
    modelo de examenes

---
+++ app/tbl_examenes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_examenes extends Model
+{
+  protected $primaryKey = 'exmm_id';
+  protected $fillable = ['exmm_Nombre', 'exmm_Descripcion'];
+  protected $dates = ['created_at', 'updated_at'];
+  
+}
