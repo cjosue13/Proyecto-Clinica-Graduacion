2491463f47409d3ece46b45cd48dcc9ada4e5158
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-09-17 17:38:23 -0600

    act #28
    modelo de etapas

---
+++ app/tbl_etapas.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tbl_etapas extends Model
+{
+  protected $primaryKey = 'et_id';
+  protected $fillable = ['et_nombre'];
+  protected $dates = ['created_at', 'updated_at'];
+}
