1fcc7f3877a1106a94515bf5a3b3b8208c4ffcc4
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-10-22 14:52:01 -0600

    inicial

---
+++ database/migrations/2020_10_22_203041_create_tbl_agenda_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblAgendaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_agendas', function (Blueprint $table) {
+            $table->increments('agn_id');
+            $table->string('agn_NombreCompleto', 100);
+            $table->string('agn_telefono', 30);
+            $table->date('agn_fecha');
+            $table->string('agn_HoraInicio', 10);
+            $table->string('agn_HoraFinal', 10);
+            $table->double('agn_estado', 11, 0);
+            $table->double('agn_Tiempo', 10, 0);
+            $table->string('agn_descripcion', 500);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_agenda');
+    }
+}
