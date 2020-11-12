86cdb843fec492ad991a82b3226fd6ca51321bc1
JordiRodriguezC <ycorrales120@gmail.com>
2020-11-12 16:32:15 -0600

    Guarda Personal Social
    SOLAMENTE GUARDA AÚN NO EDITA Y ESO YA LO ESTOY HACIENDO

--- app/tbl_personalessociales.php
+++ app/tbl_personalessociales.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_personalessociales extends Model
 {
   protected $primaryKey = 'ps_id';
-  protected $fillable = ['ps_fkEtapa','ps_fkExpediente','ps_descripcion'];
+  protected $fillable = ['ps_Etapa','ps_fkExpediente','ps_descripcion'];
   protected $dates = ['created_at', 'updated_at'];
 }
