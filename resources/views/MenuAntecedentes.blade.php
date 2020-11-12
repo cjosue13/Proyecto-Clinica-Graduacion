2428d027e1857ce6b3df3fd1d9874e6b05d5187c
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2020-11-12 13:54:54 -0600

    act #41

--- resources/views/MenuAntecedentes.blade.php
+++ resources/views/MenuAntecedentes.blade.php
@@ -25,6 +25,14 @@
                             {!! Form::open(['method' => 'POST','route' => ['index', $idExp, 't' ],'style'=>'display:inline']) !!}
                                 <a style=" outline:none;"><button type="submit" id="Button1" onmouseout="CambiarColor(this)" onmouseover="CambiarColor(this)" class="Button1" onclick="CambiarColor(this)"><i   style="outline:none; color: #209f85;" class="far fa-file-alt"></i><br><label class="textocool">Traumáticos</label></button></a>
                             {!! Form::close() !!}
+                            
+                            {!! Form::open(['method' => 'POST','route' => ['index', $idExp, 't' ],'style'=>'display:inline']) !!}
+                                <a style=" outline:none;"><button type="submit" id="Button1" onmouseout="CambiarColor(this)" onmouseover="CambiarColor(this)" class="Button1" onclick="CambiarColor(this)"><i   style="outline:none; color: #209f85;" class="far fa-file-alt"></i><br><label class="textocool">Consultas</label></button></a>
+                            {!! Form::close() !!}
+
+                            {!! Form::open(['method' => 'POST','route' => ['index', $idExp, 't' ],'style'=>'display:inline']) !!}
+                                <a style=" outline:none;"><button type="submit" id="Button1" onmouseout="CambiarColor(this)" onmouseover="CambiarColor(this)" class="Button1" onclick="CambiarColor(this)"><i   style="outline:none; color: #209f85;" class="far fa-file-alt"></i><br><label class="textocool">Exámenes</label></button></a>
+                            {!! Form::close() !!}
                                 <a style=" outline:none;" href="{{ route('usuarios.create')}}"><button id="Button1" onmouseout="CambiarColor(this)" onmouseover="CambiarColor(this)" class="Button1" onclick="CambiarColor(this)"><i     style="outline:none; color: #209f85;" class="far fa-file-alt"></i><br><label class="textocool">Patológicos</label></button></a>
                                 <a style=" outline:none;" href="{{ route('reportes') }}"><button id="Button1" onmouseout="CambiarColor(this)" onmouseover="CambiarColor(this)" class="Button1" onclick="CambiarColor(this)"><i style="outline:none; color: #209f85;" class="far fa-file-alt"></i><br><label class="textocool">No Patológicos</label></button></a>
                             </ul>
