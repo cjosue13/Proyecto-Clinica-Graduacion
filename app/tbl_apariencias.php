6f5e2b64a8375a1f10f7ffa1e834f6eee34894c8
JordiRodriguezC <ycorrales120@gmail.com>
2021-01-06 18:48:20 -0600

    :boom:

--- app/tbl_apariencias.php
+++ app/tbl_apariencias.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class tbl_apariencias extends Model
 {
   protected $primaryKey = 'apa_id';
-  protected $fillable = ['apa_Piel', 'apa_Extremidades', 'apa_SignosRespiratorios', 'apa_Nasofaringeo'];
+  protected $fillable = ['apa_examenClinico','apa_Piel', 'apa_Extremidades', 'apa_SignosRespiratorios', 'apa_Nasofaringeo'];
   protected $dates = ['created_at', 'updated_at'];
   
 }
