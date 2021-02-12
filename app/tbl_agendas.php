31b13f3cd9307f7f4ed6cab66898802c09ccea27
JordiRodriguezC <ycorrales120@gmail.com>
2021-02-12 10:55:08 -0600

    Prueba

--- app/tbl_agendas.php
+++ app/tbl_agendas.php
@@ -9,5 +9,4 @@ class tbl_agendas extends Model
   protected $primaryKey = 'agn_id';
   protected $fillable = ['agn_NombreCompleto', 'agn_telefono', 'agn_fecha', 'agn_HoraInicio', 'agn_HoraFinal', 'agn_estado', 'agn_Tiempo', 'agn_descripcion'];
   protected $dates = ['created_at', 'updated_at'];
-  
 }
