23d4a35ff5ed75745cd4711a27a8c5137b6886b6
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2021-04-11 15:14:51 -0600

    Act #88 - Pruebas de paciente
    Estas son las pruebas de todos los módulos de los pacientes
    
    vendor/bin/phpunit para correrlas

---
+++ tests/Feature/PacienteModuleTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PacienteModuleTest extends TestCase
+{
+    /**
+     * Ver la lista de pacientes
+     *
+     * @return test
+     */
+    public function test_listaPacientes()
+    {
+        $response = $this->get('/pacientes');
+
+        $response->assertStatus(500);
+    }
+
+     /**
+     * Crear paciente
+     *
+     * @return test
+     */
+    public function test_CrearPaciente()
+    {
+        $response = $this->get('/pacientes/create');
+        $response->assertStatus(200);
+    }
+
+     /**
+     * Editar un paciente
+     *
+     * @return test
+     */
+    public function test_EditarPaciente()
+    {
+        $response = $this->get('/pacientes/1/edit');
+        $response->assertStatus(500);
+    }
+
+    /**
+     * PDF de paciente
+     *
+     * @return test
+     */
+    public function test_GenerarPDFPaciente()
+    {
+        $response = $this->get('/pacientes/pdf/1');
+        $response->assertStatus(500);
+    }
+}
