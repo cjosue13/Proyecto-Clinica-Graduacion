bbde77ec6c0273d86c2af2c4dd2194610f4abbc0
JordiRodriguezC <ycorrales120@gmail.com>
2021-04-11 23:09:40 -0600

    pruebas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -53,12 +53,4 @@ class Handler extends ExceptionHandler
         return parent::render($request, $exception);
     }
 
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest('login');
-    }
 }
