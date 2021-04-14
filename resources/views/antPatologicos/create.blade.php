ade5a9d31471af68ae209c5b11ab64888e4302dd
JordiRodriguezC <ycorrales120@gmail.com>
2021-04-14 16:15:24 -0600

    botones de devolverse chao
    muchas rutas y direcciones hay que cambiar y pasar demasiadas cosas si queremos hacer esos botones y además en demasiadas vistas

--- resources/views/antPatologicos/create.blade.php
+++ resources/views/antPatologicos/create.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 @section('PageTitle', 'Pacientes')
 @section('content')
-<div class="justify-content-right">
-  <br />
-  <a class="btn btn-primary" href="{{ route('antPatologicos.index') }}"> <i class="fas fa-arrow-left"></i></a>
-</div>
 <div class="row justify-content-md-center">
   <div class="col-md-6 col-md-offset-3">
     {{ Form::open(['route'=>['storeP', $idExp], 'method'=>'POST']) }}
